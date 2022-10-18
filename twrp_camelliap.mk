bb55760aa41780e13016353982703cf06b0b84ef
YogRaJSinGh <yograjfire18@gmail.com>
2022-10-18 11:59:07 +0530

    camelliap: initial twrp tree
    Signed-off-by: YogRaJSinGh <yograjfire18@gmail.com>

---
+++ twrp_camelliap.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from camellia device
+$(call inherit-product, device/xiaomi/camelliap/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := camelliap
+PRODUCT_NAME := twrp_camelliap
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO M3 PRO 5G
+PRODUCT_MANUFACTURER := xiaomi
